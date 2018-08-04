43f8814c938079014432f71a72e681689c06c9b9
David Coulombe <David.Coulombe7@gmail.com>
2018-08-04 13:38:35 -0400

    fixed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Table } from 'reactstrap';
 
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -12,36 +13,21 @@ class App extends Component {
       clicks2: 0,
       clicks3: 0
     }
-    this.handleClick1 = this.handleClick1.bind(this);
-    this.handleClick2 = this.handleClick2.bind(this);
-    this.handleClick3 = this.handleClick3.bind(this);
-    this.decrementClick1 = this.decrementClick1.bind(this);
-    this.decrementClick2 = this.decrementClick2.bind(this);
-    this.decrementClick3 = this.decrementClick3.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.decrementClick = this.decrementClick.bind(this);
   }
 
-  handleClick1() {
-    this.setState({ clicks1: this.state.clicks1 + 1 });
-  };
+  handleClick(e) {
+    console.log(e.target);
+    this.setState({ [e.target.name]: parseInt(e.target.value) + 1 });
+    console.log(this.state);
+  }
 
-  handleClick2() {
-    this.setState({ clicks2: this.state.clicks2 + 1 });
-  };
-
-  handleClick3() {
-    this.setState({ clicks3: this.state.clicks3 + 1 });
-  };
-
-
-  decrementClick1() {
-    this.setState({ clicks1: this.state.clicks1 - 1 });
-  };
-  decrementClick2() {
-    this.setState({ clicks2: this.state.clicks2 - 1 });
-  };
-  decrementClick3() {
-    this.setState({ clicks3: this.state.clicks3 - 1 });
-  };
+  decrementClick(e) {
+    console.log(e.target);
+    this.setState({ [e.target.name]: parseInt(e.target.value) - 1 });
+    console.log(this.state);
+  }
 
   render() {
     return (
@@ -58,16 +44,16 @@ class App extends Component {
             <tr>
               <th>#</th>
               <th>
-                <Button onClick={this.handleClick1} outline color="primary">Expresso +</Button>
-                <Button onClick={this.decrementClick1} outline color="primary">Expresso -</Button>
+                <Button onClick={this.handleClick} name="clicks1" value={this.state.clicks1} outline color="primary">Expresso +</Button>
+                <Button onClick={this.decrementClick} name="clicks1" value={this.state.clicks1} outline color="primary">Expresso -</Button>
               </th>
               <th>
-                <Button onClick={this.handleClick2} outline color="secondary">Latte +</Button>
-                <Button onClick={this.decrementClick2} outline color="secondary">Latte -</Button>
+                <Button onClick={this.handleClick} name="clicks2" value={this.state.clicks2} outline color="secondary">Latte +</Button>
+                <Button onClick={this.decrementClick} name="clicks2" value={this.state.clicks2} outline color="primary">Latte -</Button>
               </th>
               <th>
-                <Button onClick={this.handleClick3} color="danger">Machiato +</Button>
-                <Button onClick={this.decrementClick3} color="danger">Machiato -</Button>
+                <Button onClick={this.handleClick} name="clicks3" value={this.state.clicks3} color="danger">Machiato +</Button>
+                <Button onClick={this.decrementClick} name="clicks3" value={this.state.clicks3} outline color="primary">Machiato -</Button>
               </th>
             </tr>
 
